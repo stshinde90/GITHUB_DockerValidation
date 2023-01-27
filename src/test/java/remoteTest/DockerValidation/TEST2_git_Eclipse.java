8d9f024bdea3b739a4b9dca105985a0b1a7f79eb
Santosh Shinde <jshinds4@jci.com>
2023-01-27 11:50:31 +0530

    Committing changes to branch x

--- src/test/java/remoteTest/DockerValidation/TEST2_git_Eclipse.java
+++ src/test/java/remoteTest/DockerValidation/TEST2_git_Eclipse.java
@@ -1,1 +1,12 @@
-//Check in to eclipse
+package remoteTest.DockerValidation;
+
+public class TEST2_git_Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Updating Test 2 file and pushing to master");
+
+	}
+
+}
