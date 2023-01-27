11af98b546e481c24d392d74ebd4c34aee334749
Santosh Shinde <jshinds4@jci.com>
2023-01-27 10:56:46 +0530

    Committing new file to Branch X

---
+++ src/test/java/remoteTest/DockerValidation/TEST1_git.java
@@ -0,0 +1,12 @@
+package remoteTest.DockerValidation;
+
+public class TEST1_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Adding file to Git Hub");
+
+	}
+
+}
